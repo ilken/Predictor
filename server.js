a673ae54354d2eda8872825cc98b4d151a9ca329
Ilken Bahcecioglu <ilken.bahcecioglu@playtech.com>
2016-09-29 11:30:42 +0100

    Test script to update firebase db regularly

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require("express");
+var firebase = require("firebase");
 var app = express();
 var port = 3000;
+
+var fireBaseConfig = {
+  apiKey: "AIzaSyAE2Im6UXhsv4hbrgytHNIr7oZhFoKv1HU",
+  authDomain: "simplifyfootball.firebaseapp.com",
+  databaseURL: "https://simplifyfootball.firebaseio.com",
+  storageBucket: "simplifyfootball.appspot.com",
+  messagingSenderId: "205929549115"
+};
+
+firebase.initializeApp(fireBaseConfig);
 
 app.listen(port, function(err, res){
   if(err){
@@ -8,6 +19,24 @@ app.listen(port, function(err, res){
       console.log(err);
   }
   else{
+    var dbRef = firebase.database().ref().child('matches/0/en/0');
+    dbRef.on("value", function(snapshot) {
+        console.log(snapshot.val());
+    });
+
+    this.newScore = 0;
+    setInterval(function(){
+      this.newScore++;
+      dbRef.set({
+        date: "2016-08-13",
+        score1: this.newScore,
+        score2: 3,
+        team1: { code: 'EVE', key: 'everton', name: 'Everton' },
+        team2: { code: 'TOT', key: 'tottenham', name: 'Tottenham Hotspur' },
+        time: '15:00'
+      })
+    }.bind(this), 5000);
+
     console.log("Server started!");
   }
 });
